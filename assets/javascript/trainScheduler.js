ba2bd401049b8bb504e6d87f17baa32eab3906b6
Olga Karamyseva <olgakaramyseva@Olgas-MacBook-Pro.local>
2019-03-09 23:57:25 -0800

    Update

--- assets/javascript/trainScheduler.js
+++ assets/javascript/trainScheduler.js
@@ -37,20 +37,17 @@ $("#submit-btn").on("click", function(){
 ref.on("child_added", function(childSnapshot) {
 
     var time = childSnapshot.val().time;
-    var date = getDate(time);
-    console.log("time:" + time);
-
     var name = childSnapshot.val().name;
     var dest = childSnapshot.val().dest;
     var freq = childSnapshot.val().freq;
-    var arrival = time;
-    var mins = moment.unix(time, "HH:mm").fromNow();
+    var arrival = getArrival(time, freq);
+    var mins = moment(arrival, "HH:mm").fromNow();
 
     var newRow = $("<tr>").append(
         $("<td>").text(name),
         $("<td>").text(dest),
         $("<td>").text(freq),
-        $("<td>").text(arrival),
+        $("<td>").text(arrival.format("HH:mm")),
         $("<td>").text(mins)
       );
     
@@ -58,7 +55,43 @@ ref.on("child_added", function(childSnapshot) {
       $("#train-table > tbody").append(newRow);
 });
 
-function getDate(time) {
-    var day = moment.unix().format("YYMMDD");
-    return moment.unix((day + time), "YYMMDDHH:mm");
+function getArrival(time, freq) {
+    var day = moment().format("YYYYMMDD");
+    var date = moment((day + time), "YYYYMMDDHH:mm");
+    var hr = moment().format("HH");
+    var hr1st = date.format("HH");
+    var min = parseInt(date.format("mm"));
+
+    var added = 0;
+    var times = [];
+
+    for (i=0; freq*i<60; i++) {
+        newTime = min + freq * i;
+
+        if (newTime >= 60) {
+            newTime -= 60;
+        }
+
+        var m = moment((day + hr + newTime), "YYYYMMDDHH:mm");
+        
+        if (m < moment()) {
+            m = moment((day + hr1st + newTime), "YYYYMMDDHH:mm");
+            times.push(m.add(1, 'days'));
+        } else {
+            times.push(m);            
+        }
+
+    }
+
+    var closest = times[0];
+    for (i=1; i<times.length; i++) {
+        var dif = times[i] - moment();
+        var dif2 = times[i-1] - moment();
+        if (dif < dif2) {
+            closest = times[i];
+        }
+    }
+    
+
+    return closest;
 }
